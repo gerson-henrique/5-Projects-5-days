2b1898dff43bc3b29bce0fa3b63fd4ae6dd4f28d
Gero <gersonhoa@gmail.com>
2022-06-16 20:35:50 -0300

    feat : create app.styles.ts

---
+++ todo-list/src/App.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+const mainColor: string = '#17181f';
+const fontColor: string = '#797a81';
+
+
+export const Container = styled.div`
+background-color: ${mainColor};
+color: ${fontColor};
+`;
+
