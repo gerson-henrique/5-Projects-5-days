8214082441e6b29d3952a9f312cf2be47a70b417
Gero <gersonhoa@gmail.com>
2022-06-16 21:09:54 -0300

    feat : create list generation logic

--- todo-list/src/App.styles.ts
+++ todo-list/src/App.styles.ts
@@ -6,5 +6,17 @@ const fontColor: string = '#797a81';
 export const Container = styled.div`
 background-color: ${mainColor};
 color: ${fontColor};
-`;
+min-height: 100vh;`;
 
+export const WorkArea = styled.div`
+margin:auto;
+max-width: 900px;
+padding: 10px;`;
+
+export const Header = styled.h1`
+margin:0;
+padding:0;
+color:white;
+text-align: center;
+border-bottom: 1px solid #444;
+padding-bottom: 20px`
--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,10 +1,28 @@
-import * as Element from './App.styles'
+import { useState } from 'react'
+import * as E from './App.styles'
+import { Item } from './types/item';
+
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,
+      name: "estilizar esse desafio",
+      done: false,
+    },
+    {
+      id: 2,
+      name: "postar projeto",
+      done: true,
+    }
+  ]);
 return (
-  <Element.Container>
-    ...
-  </Element.Container>
+  <E.Container>
+    <E.WorkArea>
+      <E.Header>Lista de Tarefas</E.Header>
+      {list.map((item,ix) => (<>...</>))}
+    </E.WorkArea>
+  </E.Container>
 )
 }
 
